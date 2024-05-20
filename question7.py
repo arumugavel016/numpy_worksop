a7a386e5400fb4398c9f79d688099cdf17ae9d81
arumugavel016 <arumugavel.k2023ai-ds@sece.ac.in>
2024-05-20 14:49:25 +0530

    answer added successfully

--- question7.py
+++ question7.py
@@ -6,6 +6,22 @@ c=[7,5,3,2,3]
 d=[2]
 e=[3]
 
+# create a list as below
+output=[[1,2,3],[4,5,6],[7,5,3,2,3,[2,[3]]]]
+a=[1,2,3]
+b=[4,5,6]
+c=[7,5,3,2,3]
+d=[2]
+e=[3]
+out=[]
+out.append(a)
+out.append(b)
+d.append(e)
+c.append(d)
+out.append(c)
+print(out)
+
+
 # use these lists to create another list which will look like the list output in line 2
 
 # use append method to do this.
